a133f749d393830143b74c225c1664406202746b
rohan <rohanrajgautam@gmail.com>
2020-08-24 15:56:59 +0545

    Updates

---
+++ precache-manifest.9b43f191e3a10d0c58aa846504a8ec4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608884337e08685c3b65dd320c790ee9",
+    "url": "/hoc-demo/index.html"
+  },
+  {
+    "revision": "3c28e806d5e6da99972f",
+    "url": "/hoc-demo/static/css/main.a6b5e4e8.chunk.css"
+  },
+  {
+    "revision": "de7069efd7f512bafe9b",
+    "url": "/hoc-demo/static/js/2.23bc132e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoc-demo/static/js/2.23bc132e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c28e806d5e6da99972f",
+    "url": "/hoc-demo/static/js/main.921fe56b.chunk.js"
+  },
+  {
+    "revision": "47749f9700261c5659e3",
+    "url": "/hoc-demo/static/js/runtime-main.10cab740.js"
+  }
+]);
